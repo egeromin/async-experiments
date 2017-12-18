72f7ff7d473c6ec40b16d941b46b94bb75462079
Emanuel Geromin <egeromin@gmail.com>
2017-12-18 19:55:50 +0100

    Completed email cruncher.

--- python/kombu/spammer.py
+++ python/kombu/spammer.py
@@ -1,8 +1,10 @@
+import smtplib
 from argparse import ArgumentParser
+import dns.resolver
 
 from kombu import Connection, Consumer, Queue
 
-from config import get_rabbit_url, spam_domain
+from config import get_rabbit_url, spam_domain, spam_sender
 from exchanges import spammer_exchange
 
 
@@ -10,11 +12,36 @@ class Spammer:
 
     def __init__(self, name):
         self.name = name
+        self.find_mx_record()
+        self.msg = """Dear Colleague,
+        
+Some interesting information about you was found by the Email Cruncher. Do take a look.
+
+Best,
+
+The Email Cruncher
+"""
+
+    def find_mx_record(self):
+        resolver = dns.resolver.Resolver()
+        records = resolver.query(spam_domain, 'MX')
+        mx_record = records[0].exchange
+        self.mx_record = str(mx_record).strip().lower()
+        print("Set MX record {}".format(self.mx_record))
+
+    def send_email(self, email):
+        server = smtplib.SMTP(self.mx_record)
+        print(server.ehlo('emanuelgeromin.com'))
+        print(server.sendmail(spam_sender, [email], self.msg))
+        server.quit()
 
     def spam(self, body, message):
-        print('About to spam {}@{}'.format(
-            self.name, spam_domain
-        ))
+        email = "{}@{}".format(self.name, spam_domain)
+        print('About to spam {}'.format(email))
+        try:
+            self.send_email(email)
+        except Exception as exc:
+            print("Got exception {} when sending email, ignoring", exc)
         message.ack()
 
 
